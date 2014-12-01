23ab83770cf5a7063a1308d035b4fbf07be3baad
Apollo Clark <apolloclark@gmail.com>
2014-11-30 20:14:11 -0500

    Vagrant update

--- www/run_tests.sh
+++ www/run_tests.sh
@@ -4,22 +4,18 @@
 cd /var/www
 
 # run tests
-# echo "  Running test..."
-# pylint -f parseable project/ > pylint.out
-# nosetests --with-xcoverage --with-xunit --all-modules --traverse-namespace --cover-package=project --cover-inclusive --cover-erase -x tests.py > /dev/null
-# clonedigger --cpd-output -o clonedigger.xml project > /dev/null
+pylint -f parseable project/ > pylint.out
+nosetests --with-xcoverage --with-xunit --all-modules --traverse-namespace --cover-package=project --cover-inclusive --cover-erase -x tests.py > /dev/null
+clonedigger --cpd-output -o clonedigger.xml project > /dev/null
 sloccount --duplicates --wide --details . | fgrep -v .svn > sloccount.sc || :
-# echo "  Tests finished..."
 
 # copy over test results
-# echo "  Copying test results to Jenkins..."
 TEST_FOLDER="vagrant@192.168.1.2:/var/lib/jenkins/jobs/discover-flask-vagrant/workspace/"
 export TEST_FOLDER
-# sshpass -p "vagrant" scp -oStrictHostKeyChecking=no pylint.out $TEST_FOLDER
-# sshpass -p "vagrant" scp -oStrictHostKeyChecking=no nosetests.xml $TEST_FOLDER
-# sshpass -p "vagrant" scp -oStrictHostKeyChecking=no coverage.xml $TEST_FOLDER
-# sshpass -p "vagrant" scp -oStrictHostKeyChecking=no clonedigger.xml $TEST_FOLDER
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no pylint.out $TEST_FOLDER
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no nosetests.xml $TEST_FOLDER
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no coverage.xml $TEST_FOLDER
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no clonedigger.xml $TEST_FOLDER
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no sloccount.sc $TEST_FOLDER
-# echo "  Tests copied over to Jenkins..."
 
 exit 0
